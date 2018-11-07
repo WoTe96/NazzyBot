7b32707a471c9e7a02feb83b4c1f9b8d646dfca0
salamshakur <salamshakur@knights.ucf.edu>
2018-11-07 11:12:50 -0500

    commit #2 - Added code to get ID of tagged user, trying to see if bot

--- commands/poke.js
+++ commands/poke.js
@@ -1,7 +1,22 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+
 module.exports = {
   name: 'poke',
 	description: 'Wanna spawn pokemons while away? I got you! (Requires Pokecord Bot)',
 	async execute(message, args) {
-		  
+		  var mem = message.mentions.members.first();
+      var id = mem.id;
+      var nam = mem.username;
+
+      //console.log(nam + ': ' + id);
+
+      if(bot.users.get(id) != bot.users.bot){
+        message.channel.send('Error: Not Pokecord Bot! Command Aborted.');
+        return;
+      }
+
 	},
 };
